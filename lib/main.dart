442a5fb06f26e3df56bcef0570b1879dd1e96541
Michał Czelusta <michal.czelusta.dev@gmail.com>
2026-03-24 19:07:39 +0100

    lab_04 krakflow task list

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,80 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
+
+  List<Task> tasks = [
+    Task(title: "Sprawdzian", deadline: "jutro"),
+    Task(title: "Silownia", deadline: "dzisiaj"),
+    Task(title: "Impreza", deadline: "w tym tygodniu"),
+    Task(title: "Wakacje", deadline: "w wakacje"),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("KrakFlow"),
+      home: Center(
+
+
+        child: Column(
+          children: [
+            Text("Dzisiejsze zadania",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                ),
+              ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: tasks.length,
+                itemBuilder: (context, index) {
+                  final task = tasks[index];
+                  return TaskCard(
+                    title: task.title,
+                    subtitle: "termin: ${task.deadline}",
+                    icon: Icons.radio_button_unchecked,
+                  );
+                },
+              ),
+            ),
+          ],
         ),
-        body: Center(
-            child: Column(
-              children: [
-                Text("KrakFlow"),
-                Text("Organizacja studiów"),
-                Text("Dzisiejsze zadania"),
-              ],
-          ),
-        ),
+
+
+
+
       ),
     );
   }
 }
+
+class Task {
+  final String title;
+  final String deadline;
+  Task({required this.title, required this.deadline});
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(icon),
+        title: Text(title),
+        subtitle: Text(subtitle),
+      ),
+    );
+  }
+}
+
 
